78a5a323e84a6853f8a7fda528cdd6bf09dce5e1
Anna Kopylova <anna.metisova@gmail.com>
2023-04-10 16:33:47 +0200

    circled and crosses added on click to the first 10 buttons

--- hra.js
+++ hra.js
@@ -1,12 +1,46 @@
 let currentPlayer = 'circle';
 
-document.querySelector('button:nth-child(1)').addEventListener('click',);
-document.querySelector('button:nth-child(2)').addEventListener('click',);
-document.querySelector('button:nth-child(3)').addEventListener('click',);
-document.querySelector('button:nth-child(4)').addEventListener('click',);
-document.querySelector('button:nth-child(5)').addEventListener('click',);
-document.querySelector('button:nth-child(6)').addEventListener('click',);
-document.querySelector('button:nth-child(7)').addEventListener('click',);
-document.querySelector('button:nth-child(8)').addEventListener('click',);
-document.querySelector('button:nth-child(9)').addEventListener('click',);
-document.querySelector('button:nth-child(10)').addEventListener('click',);
+const addClass = (event) => {
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('board__field--circle');
+    return (currentPlayer = 'cross');
+  } else if (currentPlayer === 'cross') {
+    event.target.classList.add('board__field--cross');
+    return (currentPlayer = 'circle');
+  }
+};
+
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', addClass);
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', addClass);
+
+/*   document
+  .querySelectorAll('button:nth-child(-n+10)')
+  .forEach(button => button.addEventListener('click', addClass)); */
