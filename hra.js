0cbd4b64454175fb15863b231d8965c952f39c29
Anna Kopylova <anna.metisova@gmail.com>
2023-04-10 15:41:33 +0200

    added a click event listener for 10 first buttons

--- hra.js
+++ hra.js
@@ -1,1 +1,12 @@
 let currentPlayer = 'circle';
+
+document.querySelector('button:nth-child(1)').addEventListener('click',);
+document.querySelector('button:nth-child(2)').addEventListener('click',);
+document.querySelector('button:nth-child(3)').addEventListener('click',);
+document.querySelector('button:nth-child(4)').addEventListener('click',);
+document.querySelector('button:nth-child(5)').addEventListener('click',);
+document.querySelector('button:nth-child(6)').addEventListener('click',);
+document.querySelector('button:nth-child(7)').addEventListener('click',);
+document.querySelector('button:nth-child(8)').addEventListener('click',);
+document.querySelector('button:nth-child(9)').addEventListener('click',);
+document.querySelector('button:nth-child(10)').addEventListener('click',);
