07b00481a2d1427ca1f78f833dbb4356cc99851b
Anna Kopylova <anna.metisova@gmail.com>
2023-04-10 18:43:58 +0200

    added current player status that is changing with each move

--- hra.js
+++ hra.js
@@ -1,11 +1,14 @@
 let currentPlayer = 'circle';
+const playerIcon = document.getElementById('player_icon')
 
 const addClass = (event) => {
   if (currentPlayer === 'circle') {
     event.target.classList.add('board__field--circle');
+    playerIcon.classList.replace('circle', 'cross');
     return (currentPlayer = 'cross');
   } else if (currentPlayer === 'cross') {
     event.target.classList.add('board__field--cross');
+    playerIcon.classList.replace('cross', 'circle');
     return (currentPlayer = 'circle');
   }
 };
