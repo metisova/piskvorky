46549c31e637e882beea7a78aba137fb60710627
Anna Kopylova <anna.metisova@gmail.com>
2023-04-10 18:54:51 +0200

    disabled possibility to click on one square field twice

--- hra.js
+++ hra.js
@@ -1,14 +1,16 @@
 let currentPlayer = 'circle';
-const playerIcon = document.getElementById('player_icon')
+const playerIcon = document.getElementById('player_icon');
 
 const addClass = (event) => {
   if (currentPlayer === 'circle') {
     event.target.classList.add('board__field--circle');
     playerIcon.classList.replace('circle', 'cross');
+    event.target.disabled = true
     return (currentPlayer = 'cross');
   } else if (currentPlayer === 'cross') {
     event.target.classList.add('board__field--cross');
     playerIcon.classList.replace('cross', 'circle');
+    event.target.disabled = true
     return (currentPlayer = 'circle');
   }
 };
