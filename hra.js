5ce12f3bd41ce9fabcf1c114141ba765a54c7ebd
Anna Kopylova <anna.metisova@gmail.com>
2023-04-21 21:34:36 +0200

    Added squareArray to all squares of the game field, mapped each square to 'o', 'x', or '_'

--- hra.js
+++ hra.js
@@ -1,20 +1,33 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
+
 const playerIcon = document.getElementById('player_icon');
+
+const squares = document.querySelectorAll('.square');
 
 const addClass = (event) => {
   if (currentPlayer === 'circle') {
     event.target.classList.add('board__field--circle');
     playerIcon.classList.replace('circle', 'cross');
     event.target.disabled = true;
-    return (currentPlayer = 'cross');
+    currentPlayer = 'cross';
   } else if (currentPlayer === 'cross') {
     event.target.classList.add('board__field--cross');
     playerIcon.classList.replace('cross', 'circle');
     event.target.disabled = true;
-    return (currentPlayer = 'circle');
+    currentPlayer = 'circle';
   }
+
+  const squaresArray = Array.from(squares).map((button) => {
+    if (button.classList.contains('board__field--circle')) {
+      return 'o';
+    } else if (button.classList.contains('board__field--cross')) {
+      return 'x';
+    } else {
+      return '_';
+    }
+  });
 };
 
 const confirmReload = (event) => {
@@ -29,4 +42,4 @@ document.querySelector('.restart').addEventListener('click', confirmReload);
 
 document
   .querySelectorAll('.square')
-  .forEach((button) => button.addEventListener('click', addClass));
+  .forEach((btn) => btn.addEventListener('click', addClass));
